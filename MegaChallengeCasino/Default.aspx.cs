77f87c50efb7d658d2ded7b08dcca393ac2fe23e
MikeyUchiha <mikey@starfantasygames.com>
2015-05-11 23:55:49 -0700

    Added more helper methods to complete assignment
    Added helper methods to validate a bet and make sure that it's filled
    out appropriately, check the value of the pull depending on the reel,
    count the number of cherries, and handling ViewStates for the player's
    money.

--- MegaChallengeCasino/Default.aspx.cs
+++ MegaChallengeCasino/Default.aspx.cs
@@ -11,18 +11,25 @@ namespace MegaChallengeCasino
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            if(!Page.IsPostBack)
+            {
+                string[] reels = new string[] { generateRandomImage(), generateRandomImage(), generateRandomImage() };
+                displayImages(reels);
+                ViewState.Add("Money", 100);
+                outputAmountOfMoney();
+            }
         }
 
         protected void pullLeverButton_Click(object sender, EventArgs e)
         {
-            displayImage(reelOneImage);
-            displayImage(reelTwoImage);
-            displayImage(reelThreeImage);
+            double bet = validateBet();
+            double outcome = pullLever();
+            outputMessage(bet, outcome);
+            changeAmountOfMoney(bet, outcome);
+            outputAmountOfMoney();
         }
 
         Random random = new Random();
-        string[] reels = new string[3];
 
         private string generateRandomImage()
         {
@@ -31,33 +38,103 @@ namespace MegaChallengeCasino
             return images[random.Next(11)];
         }
 
-        private string selectImage(string imageName)
+        private void displayImages(string[] reels)
         {
-            string imageURL = "~/Images/" + imageName + ".png";
-            return imageURL;
+            reelOneImage.ImageUrl = "~/Images/" + reels[0] + ".png";
+            reelTwoImage.ImageUrl = "~/Images/" + reels[1] + ".png";
+            reelThreeImage.ImageUrl = "~/Images/" + reels[2] + ".png";
         }
 
-        private void displayImage(Image reelName)
+
+        private double pullLever()
         {
-            string imageName = generateRandomImage();
-            string imageURL = selectImage(imageName);
-            addImageToArray(imageName, reelName);
-            reelName.ImageUrl = imageURL;
+            string[] reels = new string[3] { generateRandomImage(), generateRandomImage(), generateRandomImage() };
+            displayImages(reels);
+
+            double bet = validateBet();
+            int multiplier = valueOfPull(reels);
+            return bet * multiplier;
         }
 
-        private void addImageToArray(string imageName, Image reelName)
+
+        private double validateBet()
         {
-            if (reelName.ID == "reelOneImage")
-                reels[0] = imageName;
-            else if (reelName.ID == "reelTwoImage")
-                reels[1] = imageName;
-            else if (reelName.ID == "reelThreeImage")
-                reels[2] = imageName;
+            double bet = 0;
+            if (!double.TryParse(betTextBox.Text, out bet)) return bet;
+            return bet;
         }
 
-        private void valueOfPull()
+        private int valueOfPull(string[] reels)
         {
+            if (hasBar(reels)) return 0;
+            if (isJackPot(reels)) return 100;
 
+            int multiplier = 0;
+            if (hasCherries(reels, out multiplier)) return multiplier;
+
+            return 0;
+        }
+
+        private bool hasBar(string[] reels)
+        {
+            for(int i = 0; i < reels.Length; i++)
+            {
+                if (reels[i] == "Bar") return true;
+            }
+            return false;
+        }
+
+        private bool isJackPot(string[] reels)
+        {
+            if (reels[0] == "Seven" & reels[1] == "Seven" & reels[2] == "Seven")
+                return true;
+            return false;
+        }
+
+        private bool hasCherries(string[] reels, out int multiplier)
+        {
+            multiplier = calculateMultiplier(reels);
+            if (multiplier > 0) return true;
+            return false;
+        }
+
+        private int countCherries(string[] reels)
+        {
+            int cherryCount = 0;
+            for (int i = 0; i < reels.Length; i++)
+            {
+                if (reels[i] == "Cherry") cherryCount++;
+            }
+            return cherryCount;
+        }
+        private int calculateMultiplier(string[] reels)
+        {
+            int cherryCount = countCherries(reels);
+            if (cherryCount == 1) return 2;
+            if (cherryCount == 2) return 3;
+            if (cherryCount == 3) return 4;
+            return 0;
+        }
+
+        private void outputMessage(double bet, double outcome)
+        {
+            if (outcome > 0)
+                resultLabel.Text = String.Format("You bet {0:C} and won {1:C}", bet, outcome);
+            else
+                resultLabel.Text = String.Format("Sorry, you lost {0:C}", bet);
+        }
+
+        private void outputAmountOfMoney()
+        {
+            moneyLabel.Text = String.Format("Player's Money: {0:C}", ViewState["Money"]);
+        }
+
+        private void changeAmountOfMoney(double bet, double outcome)
+        {
+            double money = double.Parse(ViewState["Money"].ToString());
+            money -= bet;
+            money += outcome;
+            ViewState.Add("Money", money);
         }
     }
 }
